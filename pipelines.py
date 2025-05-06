b5f4f2653e80bc21527a590a3ed18bcda937b1cd
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-05-06 20:40:36 +0200

    learning pytorch

--- pipelines.py
+++ pipelines.py
@@ -8,3 +8,33 @@ for item in data_loader:
 data_loader = DataLoader(t, batch_size=3, drop_last=False)
 for i, batch in enumerate(data_loader, 1):
     print(f'batch {i}:', batch)
+
+torch.manual_seed(1)
+t_x= torch.rand([4,3], dtype=torch.float32)
+t_y = torch.arange(4)
+
+from torch.utils.data import Dataset
+class JointDataset(Dataset):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __len__(self):
+        return len(self.x)
+    
+    def __getitem__(self, idx):
+        return self.x[idx], self.y[idx]
+    
+from torch.utils.data import TensorDataset
+joint_dataset = TensorDataset(t_x, t_y)
+
+for example in joint_dataset:
+    print(' x: ', example[0], ' y: ', example[1])
+
+joint_dataset = TensorDataset(t_x, t_y)
+
+torch.manual_seed(1)
+data_loader = DataLoader(dataset=joint_dataset, batch_size=2, shuffle=True)
+
+for i, batch in enumerate(data_loader, 1):
+    print(f'batch {i}:', 'x:', batch[0], '\n      y:', batch[1])
