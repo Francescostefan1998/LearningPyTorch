30f62c51399c6e104600cb8988c1358a8fc5f1d6
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2025-05-07 12:34:00 +0200

    start building a linear regression model

--- usingTorchvision.py
+++ usingTorchvision.py
@@ -20,3 +20,16 @@ for i, (image, attributes) in islice(enumerate(celeba_dataset), 18):
     ax.set_title(f'{attributes[31]}', size=15)
 
 plt.show()
+
+mnist_dataset = torchvision.datasets.MNIST(image_path, 'train', download=True)
+assert isinstance(mnist_dataset, Dataset)
+example = next(iter(mnist_dataset))
+print(example)
+fig = plt.figure(figsize=(15, 6))
+for i, (image, label) in islice(enumerate(mnist_dataset), 10):
+    ax = fig.add_subplot(2, 5, i+1)
+    ax.set_xticks([]); ax.set_yticks([])
+    ax.imshow(image, cmap='gray_r')
+    ax.set_title(f'{label}', size=15)
+
+plt.show()
